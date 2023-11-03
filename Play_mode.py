92d1bd67a923d3349eefb91fd5afd02b7bd96ee8
KJH@Laptop <jay5136@naver.com>
2023-11-03 19:08:03 +0900

    Framework Setting

---
+++ Play_mode.py
@@ -0,0 +1,55 @@
+from pico2d import *
+import game_framework
+
+import World
+from Background import BackGround
+from player import MetaKnight
+
+
+# Game object class here
+
+def handle_events():
+    global running
+
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+        else:
+            p1.handle_event(event)
+
+
+def init():
+    global background
+    global p1
+
+    p1 = MetaKnight()
+    World.add_object(p1, 1)
+
+    background = BackGround()
+    World.add_object(background, 0)
+
+
+def finish():
+    World.clear()
+    pass
+
+
+def update():
+    World.update()
+    # delay(0.01)
+
+
+def draw():
+    clear_canvas()
+    World.render()
+    update_canvas()
+
+def pause():
+    pass
+
+def resume():
+    pass
+
