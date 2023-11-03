546377e2b0a162932baee75b710b7051a6712d41
KJH@Laptop <jay5136@naver.com>
2023-11-03 19:19:49 +0900

    resized frame with time

--- player.py
+++ player.py
@@ -1,11 +1,18 @@
-﻿# 이것은 각 상태들을 객체로 구현한 것임.
-
-
-from pico2d import get_time, load_image, SDL_KEYDOWN, SDL_KEYUP, SDLK_SPACE, SDLK_LEFT, SDLK_RIGHT, SDLK_f, SDLK_e
+﻿from pico2d import get_time, load_image, SDL_KEYDOWN, SDL_KEYUP, SDLK_SPACE, SDLK_LEFT, SDLK_RIGHT, SDLK_f, SDLK_e
 import World
+import game_framework
 
-# state event check
-# ( state event type, event value )
+PIXEL_PER_METER = (10.0 / 0.3) # 10 pixel 30 cm
+RUN_SPEED_KMPH = 20.0 # Km / Hour
+RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
+RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
+RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
+
+
+# Boy Action Speed
+TIME_PER_ACTION = 0.5
+ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
+FRAMES_PER_ACTION = 8
 
 def right_down(e):
     return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_RIGHT
@@ -71,16 +78,16 @@ class Idle:
 
     @staticmethod
     def do(p1):
-        p1.do_call_count += 1
-
-        if p1.do_call_count == 3:
-            p1.frame = (p1.frame + 1) % 4
-        p1.do_call_count %= 3
+        p1.frame = (p1.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 4
+        # p1.do_call_count += 1
+        # if p1.do_call_count == 3:
+        #     p1.frame = (p1.frame + 1) % 4
+        # p1.do_call_count %= 3
 
 
     @staticmethod
     def draw(p1):
-        frame = p1.frame
+        frame = int(p1.frame)
         p_size_x = walking_focus[frame][1]
         p_size_y = 60
         p1.image.clip_draw(62 * frame + walking_focus[frame][0], 655, walking_focus[frame][1], 60, p1.x,
@@ -103,18 +110,17 @@ class Run:
 
     @staticmethod
     def do(p1):
-        p1.do_call_count += 1
-
-
-        if p1.do_call_count == 3:
-            p1.frame = (p1.frame + 1) % 7
-        p1.do_call_count = p1.do_call_count % 3
+        p1.frame = (p1.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 7
+        # p1.do_call_count += 1
+        # if p1.do_call_count == 3:
+        #     p1.frame = (p1.frame + 1) % 7
+        # p1.do_call_count = p1.do_call_count % 3
         p1.x += p1.dir * 5
         pass
 
     @staticmethod
     def draw(p1):
-        frame = p1.frame
+        frame = int(p1.frame)
         p_size_x = walking_focus[frame][1]
         p_size_y = 60
         p1.image.clip_draw(62 * frame + walking_focus[frame][0], 655, walking_focus[frame][1], 60, p1.x, p1.y, p_size_x * 2, p_size_y * 2)
@@ -135,17 +141,17 @@ class Normal_Attack:
 
     @staticmethod
     def do(p1):
-        p1.do_call_count += 1
-
-        if p1.do_call_count == 3:
-            p1.frame = (p1.frame + 1) % 5
-            if p1.frame == 4:
-                p1.state_machine.handle_event(('STOP', 0))
+        p1.frame = (p1.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 5
+        # p1.do_call_count += 1
+        # if p1.do_call_count == 3:
+        #     p1.frame = (p1.frame + 1) % 5
+        if int(p1.frame) == 4:
+            p1.state_machine.handle_event(('STOP', 0))
         p1.do_call_count = p1.do_call_count % 3
 
     @staticmethod
     def draw(p1):
-        frame = p1.frame
+        frame = int(p1.frame)
         p_size_x = Normal_Attack_focus[frame][1]
         p_size_y = 60
         p1.image.clip_draw(Normal_Attack_focus[frame][0], 480, Normal_Attack_focus[frame][1], 60, p1.x + 30, p1.y, p_size_x * 2, p_size_y * 2)
@@ -166,26 +172,26 @@ class Speed_Attack:
 
     @staticmethod
     def do(p1):
-        p1.do_call_count += 1
-
-        if p1.do_call_count == 3:
-            p1.frame = (p1.frame + 1) % 5
-            if p1.frame == 4:
-                p1.state_machine.handle_event(('STOP', 0))
+        p1.frame = (p1.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 5
+        # p1.do_call_count += 1
+        # if p1.do_call_count == 3:
+        #     p1.frame = (p1.frame + 1) % 5
+        if int(p1.frame) == 4:
+            p1.state_machine.handle_event(('STOP', 0))
         p1.do_call_count = p1.do_call_count % 3
 
 
     @staticmethod
     def draw(p1):
-        frame = p1.frame
+        frame = int(p1.frame)
         p_size_x = Speed_Attack_focus[frame][1]
         p_size_y = 60
         p1.image.clip_draw(Speed_Attack_focus[frame][0], Speed_Attack_focus[frame][2], Speed_Attack_focus[frame][1], 60, p1.x + 30, p1.y, p_size_x * 2, p_size_y * 2)
 
 
 class StateMachine:
-    def __init__(self, metaknight):
-        self.metaknight = metaknight
+    def __init__(self, meta_knight):
+        self.player = meta_knight
         self.cur_state = Idle
         self.transitions = {
             Idle: {right_down: Run, left_down: Run, left_up: Idle, right_up: Idle, F_down: Normal_Attack, E_down: Speed_Attack, E_out: Idle},
@@ -196,23 +202,23 @@ class StateMachine:
         }
 
     def start(self):
-        self.cur_state.enter(self.metaknight, ('NONE', 0))
+        self.cur_state.enter(self.player, ('NONE', 0))
 
     def update(self):
-        self.cur_state.do(self.metaknight)
+        self.cur_state.do(self.player)
 
     def handle_event(self, e):
         for check_event, next_state in self.transitions[self.cur_state].items():
             if check_event(e):
-                self.cur_state.exit(self.metaknight, e)
+                self.cur_state.exit(self.player, e)
                 self.cur_state = next_state
-                self.cur_state.enter(self.metaknight, e)
+                self.cur_state.enter(self.player, e)
                 return True
 
         return False
 
     def draw(self):
-        self.cur_state.draw(self.metaknight)
+        self.cur_state.draw(self.player)
 
 
 class MetaKnight:
